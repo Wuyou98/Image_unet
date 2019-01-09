260e42bdc13a25be5d8efe49bad92495c710a1a0
Wuyou98 <32351246+Wuyou98@users.noreply.github.com>
2019-01-09 13:55:03 +0800

    Add files via upload

---
+++ view_notes.py
@@ -0,0 +1,28 @@
+import sys, os
+absolute_path = os.path.abspath(__file__)
+DIR_DEPTH = 0
+ROOT = absolute_path
+for _ in range(DIR_DEPTH + 1):
+  ROOT = os.path.dirname(ROOT)
+  sys.path.insert(0, ROOT)
+from tframe.utils.summary_viewer.main_frame import SummaryViewer
+from tframe import local
+
+
+try:
+  notes_path = local.wizard('sum', current_dir=os.path.dirname(__file__),
+                           input_with_enter=False, max_depth=2)
+  print('>> Loading notes, please wait ...')
+except Exception as e:
+  import sys, traceback
+  traceback.print_exc(file=sys.stdout)
+  input('Press any key to quit ...')
+  raise e
+
+viewer = SummaryViewer(
+  notes_path, default_inactive_flags=(
+    'epoch',
+    'mark',
+  ))
+viewer.show()
+
